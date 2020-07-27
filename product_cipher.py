72eb1b18cbb87f4393845381776894dfdab081ef
Shreedhar <meshree4@gmail.com>
2020-07-27 14:56:35 +0530

    decryption code added.

--- product_cipher.py
+++ product_cipher.py
@@ -1,9 +1,9 @@
 import re
 
-def railfence(text):
+def railfence_encrypt(text):
     
     result= ""
-    echr =[] #even position letter list
+    echr = [] #even position letter list
     ochr = [] #odd position letter list
     
     for i in range(len(text)):
@@ -37,8 +37,45 @@ def encrypt(text, k):
     
     return result
 
-
+# function that will calculate and return multiplicative inverse of key.
+def inverse(key):
     
+    for i in range(26):
+        if (key * i) % 26 == 1:
+            return i
+
+def railfence_decrypt(ct):
+    ciphertext = ct
+
+    if len(ciphertext) % 2 != 0:
+        ciphertext += " "
+    
+    plain_text= ""
+    mid = len(ciphertext) // 2
+    
+    for i in range(0,mid):
+        plain_text = plain_text + ciphertext[i] + ciphertext[i + mid]        
+    
+    plain_text = plain_text.replace(" ","")
+    
+    return plain_text
+
+
+def muldecrypt(enc, kinv):
+    result = ""
+    
+    for i in range(len(enc)):
+        char = enc[i]
+        
+        if (char.isupper()):
+            # formula: plaintext = (ciphertext * key⁻¹) mod 26
+            result += chr(((ord(char) - 65) * kinv) % 26 + 65)
+        
+        else:
+            result += chr(((ord(char) - 97) * kinv) % 26 + 97 )
+    
+    return result        
+
 
 if __name__ == "__main__": 
     plain_text = input("Enter the plaintext: ")
@@ -48,5 +85,6 @@ if __name__ == "__main__":
     keyrange = [1,3,5,7,9,11,15,17,19,21,23,25]
     print("Enter key from range: ", keyrange)
     key = int(input())
-    ct = railfence((encrypt(plain_text, key)))
+    ct = railfence_encrypt((encrypt(plain_text, key)))
     print("Encryption (Cipher text) after applying Multiplicative Cipher and Railfence Cipher: " + ct)
+    print("Decryption (Plain text) after applying Railfence and Multiplicative Cipher: " + muldecrypt(railfence_decrypt(ct), inverse(key)))

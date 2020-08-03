bf413526e5ba62acf53654647b759c53e52c874c
Shreedhar <meshree4@gmail.com>
2020-08-03 13:29:43 +0530

    Extended Euclidean Algorithm used for multiplicative inverse. Affine cipher code corrected.

--- menudriven.py
+++ menudriven.py
@@ -69,9 +69,27 @@ def affineencrypt(text, k1, k2):
 
 #below function is used to calculate multiplicative inverse of key.
 def inverse(k1):
-    for i in range(1, 27):
-        if (k1*i) % 26 == 1:
-            return i
+    r1, r2, t1, t2 = 26, k1, 0, 1
+    
+    while r2 > 0:
+        q = r1 // r2
+        r = r1 - (q * r2)
+        r1 = r2
+        r2 = r
+        t = t1 - (q * t2)
+        t1 = t2 
+        t2 = t
+    
+    if r1 == 1:
+        r1 = t1
+        return r1 
+
+    # negative numbers are not used in cryptography so we make addition of modulo value and negative number to make it positive.        
+    elif t1 == -t1: 
+        return (26 + t1)         
+    
+    else:
+        return r1 
 
 def affinedecrypt(enc, kinv, k2):
     result = ""
@@ -81,9 +99,9 @@ def affinedecrypt(enc, kinv, k2):
             result += " "
         elif (char.isupper()):
             # formula: plaintext = ((ciphertext-key2) * key⁻¹) mod 26
-            result += chr((((ord(char) - 65)-k2) * kinv) % 26 + 65)
+            result += chr((((ord(char) - 65) - k2) * kinv) % 26 + 65)
         else:
-            result += chr((((ord(char) - 97)-k2) * kinv) % 26 + 97 )
+            result += chr((((ord(char) - 97) - k2) * kinv) % 26 + 97 )
     return result        
 
 if __name__ == "__main__": 
@@ -95,13 +113,15 @@ if __name__ == "__main__":
             plain_text = plain_text.replace('.',"")
             plain_text = re.sub(r'[0-9]','', plain_text)
             key_range = [1,3,5,7,9,11,15,17,19,21,23,25]
-            print("Enter the key pair from range sepearated by comma like(5, 2): ",key_range)
-            key1, key2 = [int(x) for x in input().split(', ')]
+            print("Enter key pair like 5, 2 from given key range: ", key_range)
+            k1, k2 = [int(x) for x in input().split(', ')]
             choice = int(input("\n1.Encryption\n2.Decryption\nEnter your choice: "))
             if choice == 1:
-                print("\nEncryption: "+affineencrypt(plain_text, key1, key2))
+                print("\nEncryption: "+ affineencrypt(plain_text, k1, k2))
             else:
-                print("Decryption: "+affinedecrypt(plain_text, inverse(key1), key2))
+                key_inv = inverse(k1)
+                ct = affineencrypt(plain_text, k1, k2)
+                print("Decryption: " + affinedecrypt(ct, key_inv, k2))
         elif choice == 2:
             plain_text = input("Enter your message: ")
             plain_text = plain_text.replace(" ","")

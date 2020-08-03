68fadebcc914bc961f2f6b9c160cb85215ca88bc
Shreedhar <meshree4@gmail.com>
2020-08-03 13:29:09 +0530

    Extended Euclidean Algorithm used for multiplicative inverse.

--- affinecipher.py
+++ affinecipher.py
@@ -17,9 +17,28 @@ def encrypt(text, k1, k2):
 
 #below function is used to calculate multiplicative inverse of key.
 def inverse(k1):
-    for i in range(1, 27):
-        if (k1*i) % 26 == 1:
-            return i
+    r1, r2, t1, t2 = 26, k1, 0, 1
+    
+    while r2 > 0:
+        q = r1 // r2
+        r = r1 - (q * r2)
+        r1 = r2
+        r2 = r
+        t = t1 - (q * t2)
+        t1 = t2 
+        t2 = t
+        if r1 == 1:
+            r1 = t1
+            break
+
+    # negative numbers are not used in cryptography so we make addition of modulo value and negative number to make it positive.        
+    if t1 == -t1: 
+        return (26 + t1)         
+    
+    else:
+        return r1 
+
+    
 
 def decrypt(enc, kinv, k2):
     result = ""
@@ -37,7 +56,9 @@ if __name__ == "__main__":
     plain_text = plain_text.replace(" ","")
     plain_text = plain_text.replace('.',"")
     plain_text = re.sub(r'[0-9]','', plain_text)
-    k1, k2 = [int(x) for x in input("Enter the two keys (range(0-25)) sepearated by comma like(5, 2): ").split(', ')]
+    key_range = [1,3,5,7,9,11,15,17,19,21,23,25]
+    print("Enter key pair like 5, 2 from given key range: ", key_range)
+    k1, k2 = [int(x) for x in input().split(', ')]
     kinv = inverse(k1)
     enc = encrypt(plain_text, k1, k2)
     print ("Encryption (Cipher text): " + encrypt(plain_text, k1, k2))
